d2b78103bc4d5979a50228c1fbf28bc3e6944b68
Payal Sasmal <sasmalpayal@gmail.com>
2023-01-08 22:27:16 +0530

    copy to clipboard implementation is going on

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import JSONDATA from './emoji.json';
 import { useState } from 'react';
+import copy from "copy-to-clipboard"
 
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
+  const [copyText, setCopyText] = useState('');
 
   const searchHandler = (event) => {
     setSearchTerm(event.target.value);
   }
+
+  const copyTextHandler = (event) => {
+    setCopyText(event.target.value);
+    copy(copyText);
+    console.log(copyText);
+  }
+ 
+
   return (
     <div className="App">
       <h1>😍 Emoji Search App 🤤</h1>
@@ -23,7 +33,7 @@ function App() {
         return (
           <div className="emojiRow">
             <span className='description'>{val.emoji}{" "}{val.description}</span>
-            <span className='info'>Click to copy emoji</span>
+            <span value={copyText} className='info' onClick={copyTextHandler}>Click to copy emoji</span>
           </div>
         );
       })}
