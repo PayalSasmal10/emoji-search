d63d014d54041f54be5977dca1859ccada24b062
Payal Sasmal <sasmalpayal@gmail.com>
2023-01-08 22:10:48 +0530

    icon has changed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import JSONDATA from './emoji.json';
+import { useState } from 'react';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const searchHandler = (event) => {
+    setSearchTerm(event.target.value);
+  }
   return (
     <div className="App">
       <h1>😍 Emoji Search App 🤤</h1>
-      <input type="text" placeholder='Seach Emoji.....'/>
-      {JSONDATA.map((val, key) => {
+      <input type="text" placeholder='Search Emoji.....' onChange={searchHandler} />
+      {JSONDATA.filter((val) => {
+        if(searchTerm === ''){
+          return val;
+        }else if(val.description.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())){
+          return val;
+        }
+      }).map((val, key) => {
         return (
           <div className="emojiRow">
             <span className='description'>{val.emoji}{" "}{val.description}</span>
