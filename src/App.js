ed350b86bad4cfcdab1ba5ce8c75385fb765c8d8
Payal Sasmal <sasmalpayal@gmail.com>
2023-01-08 18:35:19 +0530

    emoji json is set

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import JSONDATA from './emoji.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder='Seach Emoji.....'/>
+      {JSONDATA.map((val, key) => {
+        return (
+          <p>{val.emoji}{val.description}</p>
+        );
+      })}
     </div>
   );
 }
