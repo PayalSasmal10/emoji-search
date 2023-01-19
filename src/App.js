3aa537c56c6fd9e0eb7435056fed6c065f51ee24
Payal Sasmal <sasmalpayal@gmail.com>
2023-01-19 18:37:45 +0530

    fixed the searchbar and emoji search option

--- src/App.js
+++ src/App.js
@@ -23,39 +23,45 @@ function App() {
 
   return (
     <div className="App">
-      <h1>😍 Emoji Search App 🤤</h1>
-      <input
-        type="text"
-        // value={text}
-        placeholder="Search Emoji....."
-        onChange={searchHandler}
-      />
-      {JSONDATA.filter((val) => {
-        if (searchTerm === "") {
-          return val;
-        } else if (
-          val.description
-            .toLocaleLowerCase()
-            .includes(searchTerm.toLocaleLowerCase())
-        ) {
-          return val;
-        }
-      }).map((val, key) => {
-        return (
-          <CopyToClipboard text={val.emoji} onCopy={copyTextHandler}>
-            <div className="emojiRow">
-              <span className="description">
-                {val.emoji} {val.description}
-              </span>
-              
-                <span className='info'> 
-                  {!isCopied ? 'Click to copy emoji': 'Copied'}
+      <div className="emoji-layout">
+        <h1>😍 Emoji Search App 🤤</h1>
+          <input
+            id="search"
+            type="text"
+            // value={text}
+            placeholder="Search Emoji....."
+            onChange={searchHandler}
+          />
+        
+      </div>
+      <div className="wrapping-jsondata">
+        {JSONDATA.filter((val) => {
+          if (searchTerm === "") {
+            return val;
+          } else if (
+            val.description
+              .toLocaleLowerCase()
+              .includes(searchTerm.toLocaleLowerCase())
+          ) {
+            return val;
+          }
+        }).map((val, key) => {
+          return (
+            <CopyToClipboard text={val.emoji} onCopy={copyTextHandler}>
+              <div className="emojiRow">
+                <span className="description">
+                  {val.emoji} {val.description}
+                </span>
+
+                <span className="info">
+                  {!isCopied ? "Click to copy emoji" : "Copied"}
                   {/* {isCopied ? "Copied" : ""} */}
                 </span>
-            </div>
-          </CopyToClipboard>
-        );
-      })}
+              </div>
+            </CopyToClipboard>
+          );
+        })}
+      </div>
     </div>
   );
 }
